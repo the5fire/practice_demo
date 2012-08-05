d6eb59a851b87657ada49a39166ef6c470ae7c18
the5fire <thefivefire@gmail.com>
2012-08-05 21:49:08 +0800

    基本的mvc结构

--- learn_tornado/mvc_hello/handlers/index.py
+++ learn_tornado/mvc_hello/handlers/index.py
@@ -4,6 +4,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hello, world!")
+        self.render('index.html')
+        #self.write("hello, world!")
 
 
