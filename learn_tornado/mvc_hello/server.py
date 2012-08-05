31057f32e7eb68e692488fd9bd4a3a1ff01ccf7b
the5fire <thefivefire@gmail.com>
2012-08-05 20:44:12 +0800

    add print out info

--- learn_tornado/mvc_hello/server.py
+++ learn_tornado/mvc_hello/server.py
@@ -1,8 +1,16 @@
 #coding:utf-8
 
 import tornado.ioloop
+import sys
+
 from urls import application
 
+PORT = '8080'
+
 if __name__ == "__main__":
-    application.listen(8888)
+    if len(sys.argv) > 1:
+        PORT = sys.argv[1]
+    application.listen(PORT)
+    print 'Development server is running at http://127.0.0.1:%s/' % PORT
+    print 'Quit the server with CONTROL-C'
     tornado.ioloop.IOLoop.instance().start()
