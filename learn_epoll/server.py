fd94921700bd3f11f11d7bff51c13535e9703e35
allenhu <thefivefire@gmail.com>
2014-09-17 23:08:57 +0800

    简单得socket编程

---
+++ learn_epoll/server.py
@@ -0,0 +1,22 @@
+import socket
+
+EOL1 = b'\n\n'
+EOL2 = b'\n\r\n'
+response = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
+response += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
+response += b'Hello, world!'
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.bind(('0.0.0.0', 8080))
+serversocket.listen(1)
+
+client, address = serversocket.accept()
+request = b''
+while EOL1 not in request and EOL2 not in request:
+    request += client.recv(1024)
+print request.decode()
+client.send(response)
+client.close()
+
+serversocket.close()
