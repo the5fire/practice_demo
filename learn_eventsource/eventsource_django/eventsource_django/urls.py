8388dbb75b65e0b687912a401f8cf49776336e0c
allenhu <thefivefire@gmail.com>
2014-10-29 17:46:20 +0800

    测试两个ajax

--- learn_eventsource/eventsource_django/eventsource_django/urls.py
+++ learn_eventsource/eventsource_django/eventsource_django/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns
 
 from django.views.generic import TemplateView
 
-from .views import eventsource
+from .views import eventsource, ajax
 
 
 urlpatterns = patterns(
     '',
     (r'^eventsource/$', eventsource),
+    (r'^ajax/$', ajax),
     (r'^$', TemplateView.as_view(template_name="index.html")),
 )
--- learn_eventsource/eventsource_django/eventsource_django/views.py
+++ learn_eventsource/eventsource_django/eventsource_django/views.py
@@ -2,11 +2,12 @@
 
 import time
 
-from django.http import StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.utils.timezone import now
 
 
 def eventsource(request):
+    time.sleep(10)
     response = StreamingHttpResponse(stream_generator(), content_type="text/event-stream")
     response['Cache-Control'] = 'no-cache'
     return response
@@ -19,4 +20,9 @@ def stream_generator():
 
         # 发送数据
         yield u'data: %s\n\n' % str(now())
-        time.sleep(2)
+        time.sleep(4)
+
+
+def ajax(request):
+    time.sleep(2)
+    return HttpResponse('ajax')
